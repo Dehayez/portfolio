be89f7f733dc8fb5b4726be52bca20ea8894fbc5
Yentel De Hauwere <yentel.dh@hotmail.be>
2020-10-04 19:16:08 +0200

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
-new fullpage('#fullpage', {
-	//options here
-	autoScrolling:true,
-	scrollHorizontally: true
+new fullpage('#fullPage', {
+	autoScrolling: true,
+	navigation: true,
+	navigationPosition: 'right',
+	/* anchors: ['section1', 'setion2', 'section3', 'section4', 'section5'],
+	navigationTooltips: ['Home', 'Ecospin', 'GOT', 'YouAreIt', 'Buddy'],
+	showActiveTooltip: true */
+	scrollingSpeed: 1000, 
 });
 
-//methods
-fullpage_api.setAllowScrolling(false);
+
+$(document).ready(function () {
+
+	$('.first-button').on('click', function () {
+  
+	  $('.animated-icon1').toggleClass('open');
+	});
+	$('.second-button').on('click', function () {
+  
+	  $('.animated-icon2').toggleClass('open');
+	});
+	$('.third-button').on('click', function () {
+  
+	  $('.animated-icon3').toggleClass('open');
+	});
+  });
