7d98512f2e38a13ae1e89550f91c233ebad5f1c4
YentelDeHauwere <yentel.dh@hotmail.be>
2020-10-14 02:19:16 +0200

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,3 @@ new fullpage('#fullPage', {
 	scrollingSpeed: 1000, 
 });
 
-
-$(document).ready(function () {
-
-	$('.first-button').on('click', function () {
-  
-	  $('.animated-icon1').toggleClass('open');
-	});
-	$('.second-button').on('click', function () {
-  
-	  $('.animated-icon2').toggleClass('open');
-	});
-	$('.third-button').on('click', function () {
-  
-	  $('.animated-icon3').toggleClass('open');
-	});
-  });
